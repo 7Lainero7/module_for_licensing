4bf6f1b41ff39e71fd4363e9dffe891162079f23
Lainero <7Lainero7@gmail.com>
2026-02-25 17:14:09 +0500

    added something

--- backend_license/backend_license/settings.py
+++ backend_license/backend_license/settings.py
@@ -9,22 +9,24 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(find_dotenv())
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=t-y_v5le)$5a#im68seyt%-(bn_f9&+&+lmxn^^!*+7#k&jzq'
-API_SECRET_TOKEN = "MySuperSecretAppToken_12345_ChangeMe"
+SECRET_KEY = os.getenv('SECRET_KEY')
+API_SECRET_TOKEN = os.getenv('API_SECRET_TOKEN')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -77,11 +79,11 @@ WSGI_APPLICATION = 'backend_license.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',        # Имя базы
-        'USER': 'postgres',          # Имя пользователя
-        'PASSWORD': 'qwer', # Вставь свой пароль сюда
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
